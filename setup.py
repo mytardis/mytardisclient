cbc1540a2fb392282ffdadd4ad622e28fac02fcb
James Wettenhall <James.Wettenhall@monash.edu>
2015-11-27 13:29:56 +1100

    Now using find_packages in setup.py to find all packages.

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
+"""
+setup.py for mytardisclient
+
+python setup.py develop
+python setup.py sdist
+etc.
+"""
 from setuptools import setup
+from setuptools import find_packages
 
 import mytardisclient
 
 setup(name='mytardisclient',
-      packages=['mytardisclient'],
+      packages=find_packages(),
       version=mytardisclient.__version__,
       description='Command-line client for MyTardis API',
       author='James Wettenhall',
       author_email='james.wettenhall@monash.edu',
       url='http://github.com/wettenhj/mytardisclient',
-      download_url = 'https://github.com/wettenhj/mytardisclient/archive/0.0.5.tar.gz',
-      keywords = ['mytardis', 'REST'], # arbitrary keywords
-      classifiers = [],
+      download_url='https://github.com/wettenhj/mytardisclient/archive/0.0.5.tar.gz',
+      keywords=['mytardis', 'REST'], # arbitrary keywords
+      classifiers=[],
       license='GPL',
       entry_points={
           "console_scripts": [
