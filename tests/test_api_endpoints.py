ec27149334df308371762dcc2b2e5269f4f4de5a
James Wettenhall <james.wettenhall@gmail.com>
2019-03-26 21:07:27 +1100

    Add test for getting API schema for a model resource

--- tests/test_api_endpoints.py
+++ tests/test_api_endpoints.py
@@ -8,7 +8,7 @@ import json
 import requests_mock
 
 from mtclient.conf import config
-from mtclient.models.api import ApiEndpoint
+from mtclient.models.api import ApiEndpoint, ApiSchema
 
 config.url = "https://mytardis-test.example.com"
 config.cache_backend = "dogpile.cache.null"
@@ -40,3 +40,47 @@ def test_api_list():
         returned_endpoints = \
             {endpoint.model: endpoint.response_dict for endpoint in endpoints}
         assert returned_endpoints == mock_api_endpoints
+
+
+def test_api_get_schema():
+    """
+    Test listing the API endpoints of a remote MyTardis server
+    """
+    mock_api_schema = {
+        "allowed_detail_http_methods": [
+            "get",
+            "post"
+        ],
+        "allowed_list_http_methods": [
+            "get",
+            "post"
+        ],
+        "default_format": "application/json",
+        "default_limit": 20,
+        "fields": {
+            "description": {
+                "blank": True,
+                "default": "",
+                "nullable": False,
+                "primary_key": False,
+                "readonly": False,
+                "type": "string",
+                "unique": False,
+                "verbose_name": "description"
+            }
+        },
+        "filtering": {
+            "description": [
+                "exact"
+            ]
+        },
+        "ordering": [
+            "description"
+        ]
+    }
+    mock_api_response = json.dumps(mock_api_schema)
+    with requests_mock.Mocker() as mocker:
+        url = "%s/api/v1/dataset/schema/?format=json" % config.url
+        mocker.get(url, text=mock_api_response)
+        schema = ApiSchema.get("dataset")
+        assert schema.response_dict == mock_api_schema
