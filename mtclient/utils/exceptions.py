56909b8f9ea4a620c6619f65c4a3f82c4b1d8d97
James Wettenhall <james.wettenhall@gmail.com>
2019-03-28 14:18:48 +1100

    Remove unused exception classes

--- mtclient/utils/exceptions.py
+++ mtclient/utils/exceptions.py
@@ -9,17 +9,6 @@ class DuplicateKey(Exception):
     """
 
 
-class MultipleObjectsReturned(Exception):
-    """
-    Multiple objects returned exception.
-    """
-    def __init__(self, message, url=None, response=None):
-        super(MultipleObjectsReturned, self).__init__(message)
-
-        self.url = url
-        self.response = response
-
-
 class DoesNotExist(Exception):
     """
     Does not exist exception.
@@ -30,28 +19,6 @@ class DoesNotExist(Exception):
         self.url = url
         self.response = response
         self.model_class = model_class
-
-
-class Unauthorized(Exception):
-    """
-    Unauthorized exception.
-    """
-    def __init__(self, message, url=None, response=None):
-        super(Unauthorized, self).__init__(message)
-
-        self.url = url
-        self.response = response
-
-
-class InternalServerError(Exception):
-    """
-    Internal server exception.
-    """
-    def __init__(self, message, url=None, response=None):
-        super(InternalServerError, self).__init__(message)
-
-        self.url = url
-        self.response = response
 
 
 class SshException(Exception):
@@ -84,16 +51,6 @@ class SshControlMasterLimit(SshException):
     """
 
 
-class ScpException(SshException):
-    """
-    SCP exception.
-    """
-    def __init__(self, message, command=None, returncode=None):
-        super(ScpException, self).__init__(message)
-        self.command = command
-        self.returncode = returncode
-
-
 class NoActiveNetworkInterface(Exception):
     """
     No active network interface exception.
@@ -122,26 +79,6 @@ class InvalidFolderStructure(Exception):
     """
     Invalid folder structure exception.
     """
-
-
-class StorageBoxAttributeNotFound(Exception):
-    """
-    Storage box attribute not found exception.
-    """
-    def __init__(self, storageBox, key):
-        message = "Key '%s' not found in attributes for storage box '%s'" \
-            % (key, storageBox.GetName())
-        super(StorageBoxAttributeNotFound, self).__init__(message)
-
-
-class StorageBoxOptionNotFound(Exception):
-    """
-    Storage box option not found exception.
-    """
-    def __init__(self, storageBox, key):
-        message = "Key '%s' not found in options for storage box '%s'" \
-            % (key, storageBox.GetName())
-        super(StorageBoxOptionNotFound, self).__init__(message)
 
 
 class InvalidConfig(Exception):
